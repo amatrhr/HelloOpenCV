ee7b8ef041a512804f278f7a1d151c85581c8c5e
Charles Laurin <cl14022@outlook.com>
2021-12-19 19:00:13 -0700

    tweaking to properly detect images

--- CoinDetector0.cpp
+++ CoinDetector0.cpp
@@ -4,13 +4,16 @@
 #include <opencv2/imgproc.hpp>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+
 #include <math.h>
 using namespace cv;
 using namespace std;
 
 #define CHANNEL_NUM 3
 const string txt = ".txt";
-const int morph_size = 5;
+const int morph_size = 3;
+const double min_dist = 50, lower_thresh = 28, upper_thresh = 150;
 
 // Define our callback which we will install for
 // mouse events
@@ -60,9 +63,12 @@ Mat cannyWrap(cv::Mat& img, double low_thresh, double high_thresh)
 		findContours(cannyMat, contours0, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
 		//cout << "I found " << contours0.size() << " contours. FOOL! " << endl;
 		eps = pow((oldcontours - contours0.size()), 2);
-		if (contours0.size() > 4500) {
-			///cout << "I found  too many contours. Making lower threshold higher. " << endl;
+		Mat element = getStructuringElement(MORPH_RECT, Size(2 * morph_size + 1, 2 * morph_size + 1), Point(morph_size, morph_size));
+	
+		if (contours0.size() > 2000) {
+			cout << "I found  too many contours. Making lower threshold higher and  " << endl;
 			low_thresh += 0.5 * hilo_diff;
+			morphologyEx(img, img, MORPH_OPEN, element);
 		}
 		else if (contours0.size() < 1000) {
 			//cout << "I found too few contours. Bringing down the high threshold. " << endl;
@@ -76,16 +82,103 @@ Mat cannyWrap(cv::Mat& img, double low_thresh, double high_thresh)
 	return cannyMat;
 }
 
+vector<int> getmiddlenumber(vector<int> vec) {
+	vector<int> middlenumbers;
+
+	if ((vec.size() % 2) == 0) {
+		int uppermidpoint = (int)vec.size() / 2;
+		int lowermidpoint = uppermidpoint--;
+
+		middlenumbers.push_back(lowermidpoint);
+		middlenumbers.push_back(uppermidpoint);
+	}
+	else {
+		int midpoint = (int)floor(vec.size() / 2.0);
+		middlenumbers.push_back(midpoint);
+	}
+
+	return middlenumbers;
+}
+
+double median(vector<int> vec) {
+	double median = 0;
+	vector<int> mids = getmiddlenumber(vec);
+
+
+	for (int element : mids) {
+		median += (1.0 / mids.size()) * vec[element];
+	}
+	return median;
+}
+
+double IQR(vector<int> vec) {
+	double q1, q3, iqr;
+	vector<int>  MN;
+	sort(vec.begin(), vec.end());
+	MN = getmiddlenumber(vec);
+	vector<int>::const_iterator first = vec.begin();
+	vector<int>::const_iterator lomiddle = vec.begin() + MN.front();
+	vector<int>::const_iterator himiddle = vec.begin() + MN.back();
+	vector<int>::const_iterator last = vec.end();
+
+
+	vector<int> LH(first, lomiddle);
+	vector<int> UH(himiddle, last);
+
+	q1 = median(LH);
+	q3 = median(UH);
+	iqr = q3 - q1;
+	return iqr;
+}
 
 
 ////*** Circle detection ***
-void houghCircleWrap(cv::Mat& img, cv::Mat& refimg, string outtext, string outimage, double low_thresh = 100, double high_thresh = 300, double min_dist = 50, double min_rad = 75, double max_rad = 300)
+void houghCircleWrap(cv::Mat& img, cv::Mat& refimg, string outtext, string outimage, double low_thresh = 100, double high_thresh = 300)
 {
 	vector<cv::Vec3f> circles;
+	vector<int> radii;
+	double medradius, iqrradius;
 	ofstream outputtext{ outtext };
-	cout << "WOrking on circles for " << outimage << endl;
-	cv::HoughCircles(img, circles, cv::HOUGH_GRADIENT, 1.5, min_rad, low_thresh, high_thresh); //, min_rad, max_rad);
 
+
+	cout << "Working on circles for " << outimage << endl;
+	cv::HoughCircles(img, circles, cv::HOUGH_GRADIENT, 1.5, min_dist, low_thresh, high_thresh); //, min_rad, max_rad);
+
+	// loop to get median, IQR of circle sizes
+	if (circles.size() > 3) {
+		for (int i = 0; i < circles.size(); i++)
+		{
+			int radius = circles[i][2];
+			radii.push_back(radius);
+
+		}
+
+		medradius = median(radii);
+		cout << "checking for outlying circles out of " << circles.size() << " detected." << endl;
+		iqrradius = IQR(radii);
+
+
+		cout << medradius << " is the median radius of the circles; IQR is " << iqrradius << endl;
+
+		// clean outlier circles
+		int outliercount = 0;
+		double upperfence = medradius + 1.33 * iqrradius;
+		double lowerfence = (medradius - 1.33 * iqrradius) < 0 ? 0.0 : (medradius - 1.33 * iqrradius);
+		for (int i = 0; i < circles.size(); i++)
+		{
+			if (circles[i][2] > upperfence) {
+				outliercount++;
+			}
+			else if ((circles[i][2] < lowerfence)) {
+				outliercount++;
+			}
+		}
+		cout << "There were " << outliercount << " outliers." << endl;
+
+		if (outliercount > 1) {
+			cv::HoughCircles(img, circles, cv::HOUGH_GRADIENT, 1.5, min_dist, low_thresh, high_thresh, lowerfence, upperfence);
+		}
+	}
 	outputtext << "#Image file: " << outimage << endl;
 	outputtext << "#Circle; Center" << endl;
 	for (int i = 0; i < circles.size(); i++)
@@ -94,6 +187,7 @@ void houghCircleWrap(cv::Mat& img, cv::Mat& refimg, string outtext, string outim
 		cv::Vec3i c = circles[i];
 		cv::Point center = cv::Point(c[0], c[1]);
 		int radius = c[2];
+		radii.push_back(radius);
 		circle(refimg, center, radius, cv::Scalar(0, 255, 0), 3);
 		circle(refimg, center, 0, cv::Scalar(255, 0, 128), 3);
 		outputtext << center.x << "," << center.y << endl;
@@ -102,8 +196,8 @@ void houghCircleWrap(cv::Mat& img, cv::Mat& refimg, string outtext, string outim
 	}
 	outputtext << "#Number of coins: " << circles.size() << endl;
 
-	outputtext << "#Characteristics: " << min_dist << " " << low_thresh << " " << high_thresh << " " << min_rad << " " << max_rad << endl;
-	show("eqimage", refimg);
+	outputtext << "#Characteristics: " << min_dist << " " << low_thresh << " " << high_thresh << endl; // " " << min_rad << " " << max_rad << endl;
+	show("Identified Coins and Centers", refimg);
 	imwrite(outimage, refimg);
 
 
@@ -296,10 +390,10 @@ int main(int argc, char** argv)
 		Point2i  circ_center;
 		vector<cv::Point2i> circle_centers;
 		vector <double> f1s;
+		bool singleFile = false;
 
 
-		double lower_thresh = atof(argv[2]), upper_thresh = atof(argv[3]), min_dist = atof(argv[4]), min_rad = atof(argv[5]), max_rad = atof(argv[6]);
-
+		//double lower_thresh = atof(argv[2]), upper_thresh = atof(argv[3]);
 
 		if (!infile.bad())
 		{
@@ -317,6 +411,7 @@ int main(int argc, char** argv)
 				else {
 					image = imread(filename, IMREAD_COLOR);
 					line = filename;
+					singleFile = true;
 				}
 
 
@@ -340,19 +435,43 @@ int main(int argc, char** argv)
 				}
 
 				cout << "Starting " << line << endl;
-				bilateralFilter(image, greyimage, 36, 72, 72);
+				bilateralFilter(image, greyimage, 36, 196, 196);
 				cvtColor(greyimage, greyimage, cv::COLOR_BGR2GRAY);
 				//show("greyimage", greyimage);
 				imwrite(greyout, greyimage);
-
-				eqimage = gammaCorrection(greyimage, 1.1);
-				//show("eqimage", eqimage);
+				Scalar matmean = mean(greyimage);
+				cout << "matman" << matmean << endl;
+				equalizeHist(greyimage, greyimage);
+				if (matmean[0] < 125) {
+					// underexposed
+					eqimage = gammaCorrection(greyimage, 0.75);
+				}
+				else if (matmean[0] > 171) {
+					eqimage = gammaCorrection(greyimage, 3.25);
+				}
+				else {
+					//overexposed
+					eqimage = gammaCorrection(greyimage, 1.25);
+				}
+				
+				
+				
+				GaussianBlur(eqimage, eqimage, cv::Size(5, 5), 1.44, 1.44);
 				imwrite(eqout, eqimage);
-
-
-				medianBlur(eqimage, eqimage, 5);
+				
 
 				eqimage.copyTo(eqcopy);
+				edgeimage = cannyWrap(eqimage, lower_thresh, upper_thresh);
+				imwrite(edgeout, edgeimage);
+		
+
+				Mat element = getStructuringElement(MORPH_RECT, Size(2 * morph_size + 1, 2 * morph_size + 1), Point(morph_size, morph_size));
+				morphologyEx(edgeimage, morphimage, MORPH_CLOSE, element);
+
+				imwrite(morphout, morphimage);
+				houghCircleWrap(edgeimage, image, textout, circleout,
+					lower_thresh, upper_thresh);
+
 				// CHECK if [Name]true_circles.txt exists!
 				ifstream truecirclecheck;
 				truecirclecheck.open(truetextin);
@@ -394,21 +513,9 @@ int main(int argc, char** argv)
 				}
 
 
-
-				edgeimage = cannyWrap(eqimage, lower_thresh, upper_thresh);
-				imwrite(edgeout, edgeimage);
-				//show("edgeimage", edgeimage);
-
-				//annotateCircle(eqimage);
-
-				Mat element = getStructuringElement(MORPH_RECT, Size(2 * morph_size + 1, 2 * morph_size + 1), Point(morph_size, morph_size));
-				morphologyEx(edgeimage, morphimage, MORPH_DILATE, element);
-				imwrite(morphout, morphimage);
-				houghCircleWrap(morphimage, image, textout, circleout,
-					lower_thresh, upper_thresh, min_dist, min_rad, max_rad);
-
-				double f1 = F1Finder(textout, truetextin, 0.33 * min_rad);
+				double f1 = F1Finder(textout, truetextin, 0.5 * min_dist);
 				f1s.push_back(f1);
+
 
 				string f1string = "# best f1: " + to_string(f1);
 				cout << "F1  for " << line << ": " << f1 << endl;
@@ -419,7 +526,9 @@ int main(int argc, char** argv)
 				appendout << f1string << endl;
 
 				cout << "Done with " << line << endl;
-
+				if (singleFile) {
+					break;
+				}
 			}
 		}
 
